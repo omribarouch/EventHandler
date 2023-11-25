ed4cd7eb6c4588f114fb8d32102c4b65c36e48da
Omri Barouch <omby8888@gmail.com>
2023-11-25 20:04:56 +0200

    resources tests stubs

---
+++ test/unit/resources/test_user.py
@@ -0,0 +1,21 @@
+from test.basic_test_setup import BasicTestSetup
+
+
+class TestUserApi(BasicTestSetup):
+    def test_post_user(self):
+        pass
+
+    def test_post_user_without_username(self):
+        pass
+
+    def test_post_user_without_password(self):
+        pass
+
+    def test_post_user_without_display_name(self):
+        pass
+
+    def test_post_user_without_is_admin(self):
+        pass
+
+    def test_post_user_with_existing_username(self):
+        pass
---
+++ test/unit/resources/test_user_login.py
@@ -0,0 +1,12 @@
+from test.basic_test_setup import BasicTestSetup
+
+
+class TestUserLoginApi(BasicTestSetup):
+    def test_post_login(self):
+        pass
+
+    def test_post_login_not_existing_username(self):
+        pass
+
+    def test_post_user_incorrect_password(self):
+        pass
